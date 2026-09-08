07e8f5a5378e7311cabd8772010f483a62bff72c
Cristian Plascencia <cristianliljon20@gmail.com>
2026-09-08 12:58:52 -0600

    Initial Commit

---
+++ InterviewPrep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InterviewPrep
+//
+//  Created by Cristian Plascencia on 08/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InterviewPrep/InterviewPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewPrepApp.swift
+//  InterviewPrep
+//
+//  Created by Cristian Plascencia on 08/09/26.
+//
+
+import SwiftUI
+
+@main
+struct InterviewPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
